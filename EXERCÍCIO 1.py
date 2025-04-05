b8c55f884768f16d681443debef29de828c8b731
DaviEstevam12 <davisantoslinkedin23@gmail.com>
2025-04-05 18:01:37 -0300

    EXERCÍCIO 1
    FEITOS LETRAS: A,B,C

---
+++ EXERCÍCIO 1.py
@@ -0,0 +1,25 @@
+import math
+'''A)TESTE MAX E MIN'''
+max(3,2.8,3.9)
+3.9
+
+def media (a,b,c):
+    return (a+b+c)/3
+'''B) CÁLCULO DAS MÉDIAS'''
+media (1,2,3)
+
+max(3,2.8,3.9)
+3.9
+'''C) FUNÇAO QUE RECEBE 3 NÚMEROS E CALCULA A DIFERENÇA'''
+import math
+def media_tres (x1,x2,x3):
+    return (x1+x2+x3)/3
+
+def diferença_media (x1,x2,x3):
+    return max (x1,x2,x3) - media_tres (x1,x2,x3)
+
+diferença_media (1,2,3)
+1.0
+
+
+
